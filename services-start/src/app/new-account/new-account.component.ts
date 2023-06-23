5a0eb3edb64eadab26df9bde54f269217db11588
selenyel <selenyelyazilim@gmail.com>
2023-06-23 16:52:25 +0300

    Lesson 108: Dependency injection and using services

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,38 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../shared/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
+//providers for our new logging.service
+
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  //we will inform angular that we need this service
+  constructor(private loggingService: LoggingService){
+
+    //OR
+    //this.loggingService = inject(LoggingService);
+
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    
+
+    
+    //console.log('A server status changed, new status: ' + accountStatus);
+    // OR
+    // const service = new LoggingService();
+    // service.logStatusChange(accountStatus);
+    // OR
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
